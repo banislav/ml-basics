5191eb558841e60bce1dae43da46454b1b97b8f9
banikrukov <danyalapin@yandex.ru>
2022-10-07 01:20:15 +0500

    [MODIFY] fix kNN regression algorithm

--- knn-regression/knn-regression.py
+++ knn-regression/knn-regression.py
@@ -2,25 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.datasets import make_regression
-
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import train_test_split
+from typing import List
 
 class KnnRegressor:
-    def __init__(self, k:int=5):
+    def __init__(self, k:int=5, n_features:int=100, noise:int=50) -> None:
         self.k = k
+        self.n_features = n_features
+        self.noise = noise
     
     def load_data(self) -> None:
-        self.dataset = make_regression(n_features=1, noise=50)
-    
-    def predict(self, x:float) -> float:
-        test_x, test_y = self.dataset
-        test_array = list(zip(test_x, test_y))
-        norm_list = [np.linalg.norm(test_array[i]) for i in range(len(test_array))]
-        norm_list = sorted([float(x) for x in norm_list])
+        X, y = make_regression(n_features=self.n_features, noise=self.noise)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y)
         
-        predict_value = np.mean(norm_list[:5])
-        return predict_value
-    
-    
-regressor = KnnRegressor(k=5)
-regressor.load_data()
-regressor.predict(3)
+    def predict(self, x:List) -> float:
+        train_array = np.array(list(zip(self.X_train, self.y_train)), dtype=object)
+        predict_values = np.array([])
+        
+        if x.shape[1] != self.X_train.shape[1]:
+            raise ValueError("value x is not suitable")
+            
+        for item in x:
+            norm_list = [(np.linalg.norm(item - train_array[i][0]), train_array[i][1]) for i in range(train_array.shape[0])]
+            norm_list = sorted(norm_list, key=lambda x: x[0])
+            
+            predict_values = np.append(predict_values, np.mean(norm_list[:self.k]))
+        
+        return predict_values
